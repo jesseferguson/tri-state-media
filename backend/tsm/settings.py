d9769d0560a88b528454ad57a513f814b429a7d5
jesseferguson <120108864+jesseferguson@users.noreply.github.com>
2026-05-11 13:41:32 -0400

    initial project comit

--- backend/tsm/settings.py
+++ backend/tsm/settings.py
@@ -87,10 +87,10 @@ WSGI_APPLICATION = 'tsm.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
